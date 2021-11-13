fcd8bfbcdc5bf2b039c3f4dfcf143373c7b091a2
NaN <Nan@Nan.Nan>
2021-11-13 21:28:11 +0100

    Parallelism implemented

--- model.py
+++ model.py
@@ -16,9 +16,39 @@ class Model:
 
     def __init__(self):
         self.model: A2C = None
-        self.env = DummyVecEnv([lambda: gym.make(Model.env_name)])
         self.path: str = './training/'
         self.__saved_model_name = 'pacman'
+
+    def train(self, n_timestep: int):
+        """Train new model if none exists in 'training/saved_models/' directory.
+        Otherwise train for aditional 'timesteps' already existing model.
+
+        :param n_timestep: The number of timesteps to train
+        """
+        if os.path.exists(self.path + 'saved_models/' + self.__saved_model_name):
+            env = make_atari_env(Model.env_name, n_envs=4, seed=0)
+            env = VecFrameStack(env, n_stack=4)
+            self.model.learn(total_timesteps=n_timestep)
+            self.save(self.model)
+            del model
+            env.close()
+        else:
+            self._train_new_model(n_timestep)
+
+
+    def _train_new_model(self, n_timesteps: int):
+        """Train new model for 'n' timesteps in parallel and save it to 'training/saved_models/' directory.
+                Will DELETE old one!!!
+
+        :param n_timesteps: The number of timesteps to train
+        """
+        env = make_atari_env(Model.env_name, n_envs=4, seed=0)
+        env = VecFrameStack(env, n_stack=4)
+        new_model = A2C('CnnPolicy', env, verbose=1)
+        new_model.learn(total_timesteps=n_timesteps)
+        self.save(new_model)
+        env.close()
+        del new_model
 
     def evaluate(self, n_eval_episodes:int=5, render=True):
         """Evaluate trained 'pacman' model in 'training/saved_models/' directory on 'n' episodes.
@@ -28,21 +58,24 @@ class Model:
         """
         if self.model is None:
             self.model = self.load()
-        print(evaluate_policy(self.model, self.env, n_eval_episodes, render=render))
-
-    def train_new_model(self, n_timesteps: int):
-        """Train new model for 'n' timesteps and save it to 'training/saved_models/' directory.
-                Will DELETE old one!!!"""
-        new_model = A2C('CnnPolicy', self.env, verbose=1)
-        new_model.learn(total_timesteps=n_timesteps)
-        self.save(new_model)
-        del new_model
+        env = make_atari_env(Model.env_name, n_envs=1, seed=0)
+        env = VecFrameStack(env, n_stack=4)
+        p = evaluate_policy(self.model, env, n_eval_episodes=n_eval_episodes, render=render,
+                            return_episode_rewards=True)
+        print(p)
+        with open(self.path + "evaluated_scores.txt", "a") as f:
+            f.write(str(p) + '\n')
+        env.close()
 
     def test(self, n_episodes:int=5):
-        """Test model on 'n'(default 5) episodes."""
+        """Test model on 'n'(default 5) episodes.
+
+        :param n_episodes: The number of episodes used for testing
+        """
         if self.model is None:
             self.model = self.load()
-        env = gym.make(Model.env_name)
+        env = make_atari_env(Model.env_name, n_envs=1, seed=0)
+        env = VecFrameStack(env, n_stack=4)
         epizodes = n_episodes
         for epizode in range(1, epizodes + 1):
             obs = env.reset()
@@ -50,7 +83,7 @@ class Model:
             score = 0
             while not done:
                 env.render()
-                action, _ = self.model.predict(obs) # Make a move
+                action, _ = self.model.predict(obs)
                 obs, reward, done, info = env.step(action)
                 score += reward
             print('Epizoda:{} Score:{}'.format(epizode, score))
@@ -58,13 +91,17 @@ class Model:
 
     def load(self):
         """Try to load 'pacman' model in 'training/saved_models/' directory and store it in object atrtibute self.model."""
+        env = make_atari_env(Model.env_name, n_envs=1, seed=0)
+        env = VecFrameStack(env, n_stack=4)
         try:
-            self.model = A2C.load(os.path.join(self.path, 'saved_models', self.__saved_model_name), self.env)
+            self.model = A2C.load(os.path.join(self.path, 'saved_models', self.__saved_model_name), env)
         except FileNotFoundError:
-            raise FileNotFoundError('No such model or file: "pacman" in "saved_models" directory'
-                                    '\n\tHave to have zip file with trained model in "training/saved_models" directory')
+            raise FileNotFoundError(f'No such model or file exists: {self.__saved_model_name} in {self.path}saved_models/ directory'
+                                    f'\n\tHave to have zip file with trained model in {self.path}saved_models/ directory.')
+        finally:
+            env.close()
         return self.model
 
-    def save(self, model_to_save: A2C):
+    def save(self, model: A2C):
         """Save model to zip file in 'training/saved_models/' directory."""
-        return model_to_save.save(os.path.join(self.path, 'saved_models', self.__saved_model_name))
+        return model.save(os.path.join(self.path, 'saved_models', self.__saved_model_name))
